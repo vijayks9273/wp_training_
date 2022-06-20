62eebb8f6b0646ff469db6b9a51ba963cfe07fd2
vijayks9273 <106740016+vijayks9273@users.noreply.github.com>
2022-06-20 16:44:01 +0530

    Add files via upload
    new folder child theme

---
+++ twentyfifteen-child/template-sample.php
@@ -0,0 +1,10 @@
+
+<?php
+/**
+* Template Name: sample
+*/
+get_header();
+?>
+<h1 class="samplehead">This is sample template in this header and footer called from parent theme</h1>
+<?php
+get_footer();
